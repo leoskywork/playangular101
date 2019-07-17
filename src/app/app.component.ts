794ee2c050c10c5d6ac7680bbcf9007dd2c33c88
Leo <leoskywork@outlook.com>
2019-07-17 21:31:07 +0800

    clean up index.html and add empty clipboard component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular101';
+  title: string = 'angular 101';
+  age: number = 10;
+
+  constructor(){
+    
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClipboardComponent } from './components/clipboard/clipboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClipboardComponent
   ],
   imports: [
     BrowserModule,
