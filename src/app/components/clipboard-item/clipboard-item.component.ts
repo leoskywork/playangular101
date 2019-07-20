21a7bed9abdb3e2edd8a0a8b063cb7a106cc530a
Leo <leoskywork@outlook.com>
2019-07-20 18:03:38 +0800

    impl copy note and UI of edit note

--- src/app/components/clipboard-item/clipboard-item.component.ts
+++ src/app/components/clipboard-item/clipboard-item.component.ts
@@ -8,10 +8,55 @@ import { Note } from 'src/app/models/note';
 })
 export class ClipboardItemComponent implements OnInit {
   @Input() note: Note;
+  isEditing: boolean;
+  newData: string;
+  isSaving: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.newData = this.note.data;
+  }
+
+  onDelete() {
+
+  }
+
+  onBeginEdit() {
+    console.log('onBeginEdit()');
+    this.isEditing = true;
+  }
+
+  onCopy() {
+    console.log('onCopy() - ' + this.note.data);
+    // js only allow copy from input/textarea due to security restriction, so create a temp element node here
+    //   - https://stackoverflow.com/questions/49236100/copy-text-from-span-to-clipboard
+    const textArea = document.createElement('textarea');
+    textArea.value = this.note.data;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('Copy');
+    textArea.remove();
+    // todo - show 'Copied'
+  }
+
+  onSave() {
+    this.isSaving = true;
+    // todo - save to server
+
+  }
+
+  onCancelEdit() {
+    this.isEditing = false;
+    this.newData = this.note.data;
+  }
+
+  canEnableSave() {
+    return this.isEditing && !this.isSaving && this.newData !== this.note.data;
+  }
+
+  canEnableCancel() {
+    return !this.isSaving;
   }
 
 }
