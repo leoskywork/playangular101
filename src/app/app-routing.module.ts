8ea19e1cad479c1073cb86ad641d28852dc2087c
Leo <leoskywork@outlook.com>
2019-11-02 16:05:22 +0800

    add router-outlet and rule of routes for root path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClipboardComponent } from './components/clipboard/clipboard.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    //the root path
+    { path: '', component: ClipboardComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
