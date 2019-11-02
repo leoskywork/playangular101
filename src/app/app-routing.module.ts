f143579d60fb28aeaa1d49a9cad83cfaf67054eb
Leo <leoskywork@outlook.com>
2019-11-02 18:07:06 +0800

    add nav bar and route rules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClipboardComponent } from './components/clipboard/clipboard.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 const routes: Routes = [
     //the root path
-    { path: '', component: ClipboardComponent }
+    { path: '', component: ClipboardComponent },
+    { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -10,10 +10,17 @@ export class HeaderComponent implements OnInit {
     title: string = Constants.getAppEnv() + ' ' + Constants.appName;
     version: string = Constants.version;
     versionDate: string = Constants.versionDate;
+    currentNavItem: string = 'Note';
 
     constructor() { }
 
     ngOnInit() {
+    }
+
+    onClickNavItem(event: MouseEvent) {
+        //console.log(event);
+        const navItem = <HTMLElement>event.target;
+        this.currentNavItem = navItem.innerText;
     }
 
 }
